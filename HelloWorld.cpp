15a88a40fc8329131dbdd383ec0dce740dc59f93
Esguardian12 <esmultipro@gmail.com>
2026-05-16 09:59:48 -0400

    I added the necessary file.

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    std::cout << "I like pizza!" << std::endl;       //std::cout starts the line.         std::endl ends the line.
+    std::cout << "It's really good!" << std::endl;
+    std::cout << "I'm learning C++" << '\n';         //'\n' can be used but std::endl good to flush the ouput buffer.
+    std::cout << "Hello World" << std::endl;
+
+
+    return 0;
+}
